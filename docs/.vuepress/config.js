586c08c94d5e74bf6dc6cfd3c9299edd3a462847
ernestchu <b073040018@nsysu.edu.tw>
2021-11-10 23:54:25 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'Hello, VuePress!',
+  description: 'This is my first VuePress site',
+  base: '/course-notes/',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+}
